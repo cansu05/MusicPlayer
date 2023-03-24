be6522f291a4b78d9ced5fdaeb811ca79e09ac70
cansu <cansuque5@gmail.com>
2023-03-24 15:53:06 +0300

    constructors

---
+++ app.js
@@ -0,0 +1,26 @@
+const player = new MusicPlayer(musicList);
+
+let music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+
+player.previous();
+music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+
+
